8281053b6f753ce76d12c50b90aba8ff5be12787
J.D.Song <juude.song@gmail.com>
2015-07-22 20:09:34 +0800

    compression

--- express_demo/app.js
+++ express_demo/app.js
@@ -1,7 +1,13 @@
 var express = require('express')
 var multer  = require('multer')
+var compression = require('compression')
 var app = express()
 
+app.use(compression({filter:shouldCompress}), function(req, res){
+    console.log(req.baseUrl);
+    console.log("eehhfle")
+    res.end()
+}) 
 
 app.get('/', function (req, res) {
       res.send('Hello World!');
@@ -10,6 +16,21 @@ app.get('/', function (req, res) {
 app.get('/imageinfo', function(req, res) {
       res.send('ImageInfo is');
 });
+
+app.get('/baidu', function(req, res) {
+  res.redirect("http://baidu.com")
+});
+
+
+function shouldCompress(req, res) {
+    if (req.headers['x-no-compression']) {
+        // don't compress responses with this request header
+        return false
+    }
+    console.log(req.baseUrl)
+   // fallback to standard filter function
+   return compression.filter(req, res)
+}
 
 app.use(multer({dest:'./uploads/'}), function(req, res, next) {
     console.log(req.body)
