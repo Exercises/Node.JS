ae0bcf304b7239bec89799de7000513184525617
J.D.Song <juude.song@gmail.com>
2015-11-21 15:14:05 +0800

    add property of a object

--- basicjs/object.js
+++ basicjs/object.js
@@ -7,6 +7,8 @@ world.prototype.draw = function() {
     console.log('I am drawing');
 }
 
+
 var w = new world();
-console.log('world color: ' + w.color);
+w.hehe = "hehe";
+console.log('world color: ' + w.colori + "hehe: " + w.hehe);
 w.draw();
